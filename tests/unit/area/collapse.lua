f5436f8b91be3558c98f72ef8ae8a43691a05ca6
Bauumm <oh4@web.de>
2024-06-22 14:08:24 +0200

    fix collapse test for different scales

--- tests/unit/area/collapse.lua
+++ tests/unit/area/collapse.lua
@@ -2,6 +2,7 @@ local collapse = require("ui.area.collapse")
 local rectangle = require("ui.element.rectangle")
 local state = require("ui.state")
 local area = require("ui.area")
+local ui = require("ui")
 
 local test = {}
 
@@ -25,8 +26,8 @@ test.sequence = coroutine.create(function()
     -- should not be cut off yet, so cutout should match content bounds
     assert(collapse_state.cutout.left == area_bounds.left, "cutout not matching content despite not cut off")
     assert(collapse_state.cutout.top == area_bounds.top, "cutout not matching content despite not cut off")
-    assert(collapse_state.cutout.right == area_bounds.left + 100, "cutout not matching content despite not cut off")
-    assert(collapse_state.cutout.bottom == area_bounds.top + 100, "cutout not matching content despite not cut off")
+    assert(collapse_state.cutout.right == area_bounds.left + 100 * ui.scale, "cutout not matching content despite not cut off")
+    assert(collapse_state.cutout.bottom == area_bounds.top + 100 * ui.scale, "cutout not matching content despite not cut off")
     -- limit area in width, try multiple factors
     for fac = 0, 99 do
         collapse_state.width_factor = fac / 100
@@ -34,12 +35,12 @@ test.sequence = coroutine.create(function()
         -- should be cut off now, so cutout should no longer match content in width
         assert(collapse_state.cutout.left == area_bounds.left, "cutout wrong")
         assert(collapse_state.cutout.top == area_bounds.top, "cutout wrong")
-        assert(collapse_state.cutout.right ~= area_bounds.left + 100, "cutout still matching content in width despite factor not 1")
-        assert(collapse_state.cutout.bottom == area_bounds.top + 100, "cutout wrong")
+        assert(collapse_state.cutout.right ~= area_bounds.left + 100 * ui.scale, "cutout still matching content in width despite factor not 1")
+        assert(collapse_state.cutout.bottom == area_bounds.top + 100 * ui.scale, "cutout wrong")
         -- width should be smaller than rectangle
-        assert(area_bounds.right - area_bounds.left < 100, "area width should be smaller than contents")
+        assert(area_bounds.right - area_bounds.left < 100 * ui.scale, "area width should be smaller than contents")
         -- in fact it should correspond to content width * width factor
-        assert(area_bounds.right == area_bounds.left + 100 * collapse_state.width_factor, "area width should match content width * width factor")
+        assert(area_bounds.right == area_bounds.left + 100 * ui.scale * collapse_state.width_factor, "area width should match content width * width factor")
     end
 
     -- change direction and do the same again
@@ -50,12 +51,12 @@ test.sequence = coroutine.create(function()
         -- should be cut off now, so cutout should no longer match content in height
         assert(collapse_state.cutout.left == area_bounds.left, "cutout wrong")
         assert(collapse_state.cutout.top == area_bounds.top, "cutout wrong")
-        assert(collapse_state.cutout.right == area_bounds.left + 100, "cutout wrong")
-        assert(collapse_state.cutout.bottom ~= area_bounds.top + 100, "cutout still matching content in height despite factor not 1")
+        assert(collapse_state.cutout.right == area_bounds.left + 100 * ui.scale, "cutout wrong")
+        assert(collapse_state.cutout.bottom ~= area_bounds.top + 100 * ui.scale, "cutout still matching content in height despite factor not 1")
         -- height should be smaller than content
-        assert(area_bounds.bottom - area_bounds.top < 100, "area height should be smaller than contents")
+        assert(area_bounds.bottom - area_bounds.top < 100 * ui.scale, "area height should be smaller than contents")
         -- in fact it should correspond to content height * height factor
-        assert(area_bounds.bottom == area_bounds.top + 100 * collapse_state.height_factor, "area width should match content height * height factor")
+        assert(area_bounds.bottom == area_bounds.top + 100 * ui.scale * collapse_state.height_factor, "area width should match content height * height factor")
     end
 end)
 
