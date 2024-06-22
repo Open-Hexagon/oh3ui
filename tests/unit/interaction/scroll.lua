144964d467df5e446f9314c7df0bea31a2acac89
Bauumm <oh4@web.de>
2024-06-22 13:21:42 +0200

    add touch scroll interaction part of test

--- tests/unit/interaction/scroll.lua
+++ tests/unit/interaction/scroll.lua
@@ -34,14 +34,17 @@ function test.layout()
     overflow_outer = data.overflow
 
     utils.fake_mouse_cursor()
+    utils.fake_touch_cursors()
 end
 
 function test.teardown()
     utils.stop_mouse_control()
+    utils.stop_touch_control()
 end
 
 test.sequence = coroutine.create(function()
     utils.start_mouse_control()
+    utils.start_touch_control()
     utils.mouse_x = x
     utils.mouse_y = y
     assert(scroll_state_outer.position == 0, "position should be initialized at 0")
@@ -89,6 +92,87 @@ test.sequence = coroutine.create(function()
     -- both should be reset now
     assert(scroll_state_outer.position == 0, "outer scroll area should be scrolled back")
     assert(scroll_state_inner.position == 0, "inner scroll area should be scrolled back")
+
+    -- scroll with touch
+    local touch1 = utils.create_touch()
+    touch1.x = x + 10
+    touch1.y = y + 100
+    love.event.push("touchpressed", touch1.id, touch1.x, touch1.y, 0, 0, touch1.pressure)
+    coroutine.yield()
+    for _ = 1, 9 do
+        touch1.y = touch1.y - 10
+        coroutine.yield()
+        assert(scroll_state_outer.position == y + 100 - touch1.y, "position does not match touch position")
+    end
+
+    -- second touch takes over control
+    local touch2 = utils.create_touch()
+    touch2.x = x + 10
+    touch2.y = y + 100
+    love.event.push("touchpressed", touch2.id, touch2.x, touch2.y, 0, 0, touch2.pressure)
+    coroutine.yield()
+
+    -- position should no longer change with old finger
+    last_pos = scroll_state_outer.position
+    touch1.y = touch1.y - 10
+    coroutine.yield()
+    assert(scroll_state_outer.position == last_pos, "position should not have changed")
+
+    -- it should however with the new one
+    local start_pos = scroll_state_outer.position
+    for _ = 1, 9 do
+        touch2.y = touch2.y - 1
+        coroutine.yield()
+        assert(scroll_state_outer.position == y + 100 - touch2.y + start_pos, "position does not match touch position")
+    end
+    -- give back control to touch1
+    utils.delete_touch(touch2)
+    coroutine.yield()
+    -- it should not result in changes due to scroll velocity
+    last_pos = scroll_state_outer.position
+    coroutine.yield()
+    assert(scroll_state_outer.position == last_pos, "position should not have changed")
+
+    -- scroll back a bit
+    for _ = 1, 9 do
+        touch1.y = touch1.y + 10
+        coroutine.yield()
+    end
+
+    -- stop touching
+    utils.delete_touch(touch1)
+    -- check for scroll velocity
+    assert(scroll_state_outer.velocity > 0, "scroll velocity should be more than 0")
+    -- wait until it is no longer moving
+    last_pos = nil
+    while scroll_state_outer.position ~= last_pos do
+        last_pos = scroll_state_outer.position
+        coroutine.yield()
+    end
+    assert(scroll_state_outer.velocity == 0, "scroll velocity should be 0")
+
+    -- reset position
+    scroll_state_outer.position = 0
+
+    -- make sure touch scroll does not happen if touch is on scrollbar
+    local touch = utils.create_touch()
+    touch.x = x + 400
+    touch.y = y
+    love.event.push("touchpressed", touch.id, touch.x, touch.y, 0, 0, touch.pressure)
+    -- love always creates a mousepressed event on touch presses as well
+    utils.mouse_x = touch.x
+    utils.mouse_y = touch.y
+    love.event.push("mousepressed", touch.x, touch.y, 1, true, 1)
+    coroutine.yield()
+    for _ = 1, 100 do
+        touch.y = touch.y + 3
+        -- on touch devices the position returned by love.mouse.getPosition generally corresponds to the latest touch
+        utils.mouse_x = touch.x
+        utils.mouse_y = touch.y
+        coroutine.yield()
+    end
+    -- if touch scroll was used it'd have scrolled in the other direction (towards 0)
+    assert(scroll_state_outer.position == overflow_outer, "outer scroll area should be scrolled to threshold")
 end)
 
 return test
--- tests/utils.lua
+++ tests/utils.lua
@@ -17,6 +17,97 @@ function utils.stop_mouse_control()
     love.mouse.getPosition = monkeypatch.get_original(love.mouse.getPosition)
 end
 
+local cursor_color = { 1, 0.5, 0.5, 0.5 }
+local cursor_radius = 3
+
+function utils.fake_mouse_cursor()
+    draw_queue.rectangle(
+        "line",
+        utils.mouse_x - cursor_radius,
+        utils.mouse_y - cursor_radius,
+        utils.mouse_x + cursor_radius,
+        utils.mouse_y + cursor_radius,
+        cursor_color,
+        cursor_radius,
+        cursor_radius
+    )
+end
+
+local touches = {}
+local id_counter = 0
+
+function utils.create_touch()
+    id_counter = (id_counter + 1) % 256
+    local touch = {
+        id = id_counter,
+        pressure = 1,
+        x = 0,
+        y = 0,
+    }
+    touches[#touches + 1] = touch
+    return touch
+end
+
+function utils.delete_touch(t)
+    for i = 1, #touches do
+        if touches[i] == t then
+            table.remove(touches, i)
+            return
+        end
+    end
+end
+
+local function get_touch(id)
+    for i = 1, #touches do
+        if touches[i].id == id then
+            return touches[i]
+        end
+    end
+end
+
+function utils.start_touch_control()
+    touches = {}
+    id_counter = 0
+    love.touch.getTouches = monkeypatch.replace(love.touch.getTouches, function()
+        local ids = {}
+        for i = 1, #touches do
+            ids[i] = touches[i].id
+        end
+        return ids
+    end)
+    love.touch.getPosition = monkeypatch.replace(love.touch.getPosition, function(id)
+        local touch = get_touch(id)
+        return touch.x, touch.y
+    end)
+    love.touch.getPressure = monkeypatch.replace(love.touch.getPressure, function(id)
+        return get_touch(id).pressure
+    end)
+end
+
+function utils.stop_touch_control()
+    love.touch.getTouches = monkeypatch.get_original(love.touch.getTouches)
+    love.touch.getPosition = monkeypatch.get_original(love.touch.getPosition)
+    love.touch.getPressure = monkeypatch.get_original(love.touch.getPressure)
+end
+
+local touch_cursor_color = { 0.5, 1, 0.5, 0.5 }
+local touch_cursor_radius = 6
+
+function utils.fake_touch_cursors()
+    for i = 1, #touches do
+        draw_queue.rectangle(
+            "line",
+            touches[i].x - touch_cursor_radius,
+            touches[i].y - touch_cursor_radius,
+            touches[i].x + touch_cursor_radius,
+            touches[i].y + touch_cursor_radius,
+            touch_cursor_color,
+            touch_cursor_radius,
+            touch_cursor_radius
+        )
+    end
+end
+
 local graphic_functions = { "rectangle", "polygon", "draw" }
 
 function utils.add_graphics_callback(fun)
@@ -74,22 +165,6 @@ function utils.check_color_at(x, y, check_color)
         end
     end
     return true
-end
-
-local cursor_color = { 1, 0.5, 0.5, 0.5 }
-local cursor_radius = 3
-
-function utils.fake_mouse_cursor()
-    draw_queue.rectangle(
-        "line",
-        utils.mouse_x - cursor_radius,
-        utils.mouse_y - cursor_radius,
-        utils.mouse_x + cursor_radius,
-        utils.mouse_y + cursor_radius,
-        cursor_color,
-        cursor_radius,
-        cursor_radius
-    )
 end
 
 local log = {}
