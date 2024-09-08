a80520cfa8c03155146a40c71871186020673fd8
Syyrion <cuiricky4@gmail.com>
2024-09-07 20:34:49 -0400

    add comments

--- ui/interaction/mouse/init.lua
+++ ui/interaction/mouse/init.lua
@@ -1,6 +1,6 @@
 local events = require("ui.events")
-local output = require("ui.interaction.mouse.output").active
 local button_names = require("ui.interaction.mouse.button_names")
+local output = require("ui.interaction.mouse.output").active -- we only care about updating the active table
 
 local mouse = {
     x = -1,
@@ -11,14 +11,17 @@ local mouse = {
 
 ---Update mouse output
 function mouse.update()
+    -- Get mouse position and set previous position
     mouse.prev_x, mouse.prev_y = mouse.x, mouse.y
     mouse.x, mouse.y = love.graphics.inverseTransformPoint(love.mouse.getPosition())
 
+    -- Clear the output table
     for i = 1, button_names.length do
         output[i].up = false
         output[i].down = false
     end
 
+    -- Search for press and release events and update the output
     for event in events.iterate("mouse[pr]") do
         local name, x, y, button, istouch, presses = unpack(event)
         button = output[button]
