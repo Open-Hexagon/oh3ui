9e78164177b93a485e48612362c533e40906bc5c
Syyrion <cuiricky4@gmail.com>
2025-11-11 21:23:19 -0500

    reduce indentation, add disable mode

--- ui/control/mouse_navigation/sensor.lua
+++ ui/control/mouse_navigation/sensor.lua
@@ -66,6 +66,7 @@ local sensor_mode = {
     block = 0x1,
     lazy = 0x2,
     draggable = 0x4,
+    disable = 0x8, -- sensor cannot be hovered but can still block
 }
 
 sensor.sensor_mode = sensor_mode
@@ -97,43 +98,57 @@ function sensor.evaluate(mouse_screen_x, mouse_screen_y)
     end
     sensor.preemptive_drag_id = nil
 
-    if do_intersections then
-        -- holds the sensor id of the last encountered lazy intersection
-        local last_lazy_intersection
+    if not do_intersections then
+        return
+    end
 
-        for i = index, 1, -1 do
-            -- get sensor id, intersection mode, and bounds
-            local sensor_id = z_list[i][1]
-            local mode = z_list[i][2]
-            local x1, y1, x2, y2 = unpack(z_list[i], 3, 6)
+    -- holds the sensor id of the last encountered lazy intersection
+    local last_lazy_intersection
 
-            -- check intersection
-            if extmath.point_in_aligned_rectangle(mouse_screen_x, mouse_screen_y, x1, y1, x2, y2) then
-                -- add to hover set
-                hover_set[sensor_id] = true
+    for i = index, 1, -1 do
+        -- get sensor id, intersection mode, and bounds
+        local sensor_id = z_list[i][1]
+        local mode = z_list[i][2]
+        local x1, y1, x2, y2 = unpack(z_list[i], 3, 6)
 
-                if band(mode, sensor_mode.lazy) ~= 0 then
-                    if last_lazy_intersection then
-                        -- remove the last intersection from the hover set
-                        if last_lazy_intersection == sensor.preemptive_drag_id then
-                            sensor.preemptive_drag_id = nil
-                        end
-                        hover_set[last_lazy_intersection] = nil
-                    end
-                    last_lazy_intersection = sensor_id
-                end
-
-                -- Set the preemptive_drag_id if draggable. This overwrites the last drag id
-                if band(mode, sensor_mode.draggable) ~= 0 then
-                    sensor.preemptive_drag_id = sensor_id
-                end
-
-                if band(mode, sensor_mode.block) ~= 0 then
-                    -- checking stops if we see a blocking sensor
-                    break
-                end
-            end
+        -- check intersection, leave immediately if we aren't intersecting
+        if not extmath.point_in_aligned_rectangle(mouse_screen_x, mouse_screen_y, x1, y1, x2, y2) then
+            goto continue
         end
+
+        -- skip sensor if it's disabled; we still check if the sensor is blocking
+        if band(mode, sensor_mode.disable) ~= 0 then
+            goto continue_with_block
+        end
+
+        -- add to hover set
+        hover_set[sensor_id] = true
+
+        -- do lazy sensor checks
+        if band(mode, sensor_mode.lazy) ~= 0 then
+            -- remove the last intersection from the hover set and preemptive_drag_id if applicable
+            if last_lazy_intersection then
+                if last_lazy_intersection == sensor.preemptive_drag_id then
+                    sensor.preemptive_drag_id = nil
+                end
+                hover_set[last_lazy_intersection] = nil
+            end
+            last_lazy_intersection = sensor_id
+        end
+
+        -- Set the preemptive_drag_id if draggable. This overwrites the last drag id
+        if band(mode, sensor_mode.draggable) ~= 0 then
+            sensor.preemptive_drag_id = sensor_id
+        end
+
+        ::continue_with_block::
+
+        if band(mode, sensor_mode.block) ~= 0 then
+            -- checking stops if we see a blocking sensor
+            break
+        end
+
+        ::continue::
     end
 end
 
